e0379f8a9a9c51dc791ae6325b6c5072585555fb
Pomeo777 <31505374+Pomeo777@users.noreply.github.com>
2021-04-12 19:56:36 +0300

    Update PermissionUtil.java

--- permissionUtil/PermissionUtil.java
+++ permissionUtil/PermissionUtil.java
@@ -8,12 +8,14 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
-import timber.log.Timber;
-
+/**
+ * Created by Roman Fedchenko
+ * date 14.05.2020
+ * author email pomeo77777@gmail.com
+ */
 public class PermissionUtil {
 
     public boolean showPermissionDialog( final String permission, int myPermissionCode, Activity activity ){
-        Timber.d("showPermissionDialog(). permission = %s,  code = %s", permission, myPermissionCode);
         int currentAPIVersion = Build.VERSION.SDK_INT;
         if (currentAPIVersion >= Build.VERSION_CODES.M) {
             if (ContextCompat.checkSelfPermission(activity, permission) != PackageManager.PERMISSION_GRANTED) {
@@ -28,7 +30,6 @@ public class PermissionUtil {
     }
 
     public boolean showFragmentPermissionDialog(final String permission, int myPermissionCode, Activity activity, Fragment fragment){
-        Timber.d("showPermissionDialog(). permission = %s,  code = %s", permission, myPermissionCode);
         int currentAPIVersion = Build.VERSION.SDK_INT;
         if (currentAPIVersion >= Build.VERSION_CODES.M) {
             if (ContextCompat.checkSelfPermission(activity, permission) != PackageManager.PERMISSION_GRANTED) {
